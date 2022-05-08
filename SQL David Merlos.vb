f9e3be5c80bb71c2235bdd001cb59c182506d361
merlos1639 <104240956+merlos1639@users.noreply.github.com>
2022-05-08 02:36:12 +0200

    Add files via upload

---
+++ SQL David Merlos.vb
@@ -0,0 +1,13 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim conexion As New MySqlConnection
+        Try
+            conexion.ConnectionString = "server=localhost; user=root; password=; database=colegio"
+            conexion.Open()
+            MsgBox("CONECTADO")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
